60adc583112f1bc25de8683446d0a855cfb88584
Maxime Biloé <maxime.biloe@gmail.com>
2017-01-12 18:11:30 +0100

    // Version updated to v1.4.1

--- statsregistrations.php
+++ statsregistrations.php
@@ -36,7 +36,7 @@ class StatsRegistrations extends ModuleGraph
 	{
 		$this->name = 'statsregistrations';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.4.0';
+		$this->version = '1.4.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
