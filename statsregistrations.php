3cddfec51b77d35bc852c9502b31513374443952
Jonathan Danse <j.danse@prestaedit.com>
2021-12-29 11:22:10 +0100

    Bump version to 2.0.1

--- statsregistrations.php
+++ statsregistrations.php
@@ -37,7 +37,7 @@ class statsregistrations extends ModuleGraph
     {
         $this->name = 'statsregistrations';
         $this->tab = 'analytics_stats';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
