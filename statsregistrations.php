d965ac98216eeb6e0bf409a9dfeb55fbd72068d9
Jonathan Danse <j.danse@prestaedit.com>
2022-01-14 17:03:56 +0100

    Set tab property

--- statsregistrations.php
+++ statsregistrations.php
@@ -36,7 +36,7 @@ class statsregistrations extends ModuleGraph
     public function __construct()
     {
         $this->name = 'statsregistrations';
-        $this->tab = 'analytics_stats';
+        $this->tab = 'administration';
         $this->version = '2.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
